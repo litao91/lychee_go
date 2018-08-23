c658e6b9acc51e4918aa366b71225a913871adff
litao.91 <litao.91@bytedance.com>
2018-08-23 12:55:39 +0000

    Fix photo date and smart albums

--- modules/album.go
+++ modules/album.go
@@ -51,23 +51,77 @@ func (a *Album) PrepareData(s *LycheeServer, conn *sql.DB) (err error) {
 	return
 }
 
-func GetSmartAlbums(s *LycheeServer, conn *sql.DB) (r map[string]map[string][]string, err error) {
-	r = make(map[string]map[string][]string)
+func GetSmartAlbums(s *LycheeServer, conn *sql.DB) (r map[string]map[string]interface{}, err error) {
+	r = make(map[string]map[string]interface{})
 	// unsorted
-	unsorted := make(map[string][]string)
-	unsorted := make([]string, 0, 0)
-	rows, err := conn.Query("SELECT thumbUrl FROM lychee_photos WHERE album = 0 " + s.Settings.SortingPhotos)
+	unsorted := make(map[string]interface{})
+	unsortedThumbs := make([]string, 0, 0)
+	unsortedRows, err := conn.Query("SELECT thumbUrl FROM lychee_photos WHERE album = 0 ORDER BY " + s.Settings.SortingPhotos)
 	if err != nil {
 		log.Error("%v", err)
 		return
 	}
-	defer rows.Close()
-	for rows.Next() {
+	defer unsortedRows.Close()
+	for unsortedRows.Next() {
 		var thumbUrl string
-		rows.Scan(&thumbUrl)
-		unsorted = append(unsorted, thumbUrl)
+		unsortedRows.Scan(&thumbUrl)
+		unsortedThumbs = append(unsortedThumbs, thumbUrl)
 	}
+	unsorted["thumbs"] = unsortedThumbs
+	unsorted["num"] = len(unsortedThumbs)
 	r["unsorted"] = unsorted
+
+	// starred
+	starred := make(map[string]interface{})
+	starredThumbs := make([]string, 0, 0)
+	starredRows, err := conn.Query("SELECT thumbUrl FROM lychee_photos WHERE star = 1 ORDER BY " + s.Settings.SortingPhotos)
+	if err != nil {
+		log.Error("%v", err)
+		return
+	}
+	defer starredRows.Close()
+	for starredRows.Next() {
+		var thumbUrl string
+		starredRows.Scan(&thumbUrl)
+		starredThumbs = append(starredThumbs, thumbUrl)
+	}
+
+	starred["thumbs"] = starredThumbs
+	starred["num"] = len(starredThumbs)
+	r["starred"] = starred
+
+	publicThumbs := make([]string, 0, 0)
+	publicRows, err := conn.Query("SELECT thumbUrl From lychee_photos WHERE public = 1 ORDER BY " + s.Settings.SortingPhotos)
+	if err != nil {
+		log.Error("%v", err)
+		return
+	}
+	defer publicRows.Close()
+
+	r["public"] = gin.H{
+		"thumbs": publicThumbs,
+		"num":    len(publicThumbs),
+	}
+
+	recentThumbs := make([]string, 0, 0)
+	now := time.Now().Unix() - 24*60*60
+	recentRows, err := conn.Query("SELECT thumbUrl FROM lychee_photos WHERE id > ?", now)
+	if err != nil {
+		log.Error("%v", err)
+		return
+	}
+	defer recentRows.Close()
+	for recentRows.Next() {
+		var thumbUrl string
+		recentRows.Scan(&thumbUrl)
+		recentThumbs = append(recentThumbs, thumbUrl)
+	}
+
+	r["recent"] = gin.H{
+		"thumbs": recentThumbs,
+		"num":    len(recentThumbs),
+	}
+
 	return
 }
 
@@ -86,14 +140,14 @@ func GetAlbumsAction(server *LycheeServer, c *gin.Context) {
 	for _, a := range albums {
 		a.PrepareData(server, conn)
 	}
+	smartAlbums, err := GetSmartAlbums(server, conn)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, fmt.Sprintf("%s", err))
+		return
+	}
 	c.JSON(200, gin.H{"albums": albums,
-		"num": len(albums),
-		"smartalbums": gin.H{
-			"unsorted": gin.H{"thumbs": make([]string, 0, 0), "num": 0},
-			"starred":  gin.H{"thumbs": make([]string, 0, 0), "num": 0},
-			"recent":   gin.H{"thumbs": make([]string, 0, 0), "num": 0},
-			"public":   gin.H{"thumbs": make([]string, 0, 0), "num": 0},
-		},
+		"num":         len(albums),
+		"smartalbums": smartAlbums,
 	})
 }
 
@@ -155,35 +209,7 @@ func AddAlbumAction(server *LycheeServer, c *gin.Context) {
 	c.String(200, id)
 }
 
-func GetAlbumAction(server *LycheeServer, c *gin.Context) {
-	albumID, err := strconv.Atoi(c.PostForm("albumID"))
-	if err != nil {
-		log.Error("%v", err)
-		c.String(http.StatusBadRequest, "Invalid album id")
-		return
-	}
-	log.Debug("Get Album detail of ID %d", albumID)
-
-	conn, err := server.db.GetConnection()
-	if err != nil {
-		log.Error("%v", err)
-		c.String(http.StatusInternalServerError, "Can't connect to DB")
-		return
-	}
-	defer conn.Close()
-	album, err := GetAlbum(albumID, conn)
-	if err != nil {
-		log.Error("%v", err)
-		c.String(http.StatusBadRequest, fmt.Sprintf("%v", err))
-		return
-	}
-	album.PrepareData(server, conn)
-	photos, err := LoadPhotosOfAlbum(albumID, conn)
-	if err != nil {
-		log.Error("%v", err)
-		c.String(http.StatusInternalServerError, fmt.Sprintf("%v", err))
-		return
-	}
+func genPhotoMap(photos []*Photo) map[int64]map[string]interface{} {
 	var photoMap map[int64]map[string]interface{} = make(map[int64]map[string]interface{})
 	for i, p := range photos {
 		next := photos[(i+1)%len(photos)].ID
@@ -197,13 +223,42 @@ func GetAlbumAction(server *LycheeServer, c *gin.Context) {
 			"album":         strconv.Itoa(p.Album),
 			"thumbUrl":      p.ThumbUrl,
 			"url":           p.Url,
-			"cameraDate":    p.Takestamp,
-			"sysdate":       album.Sysdate,
 			"previousPhoto": strconv.FormatInt(prev, 10),
 			"nextPhoto":     strconv.FormatInt(next, 10),
 		}
+		if p.Takestamp != "" {
+			m["cameraDate"] = "1"
+			m["sysdate"] = p.Takestamp
+		} else {
+			m["cameraDate"] = "0"
+			t := time.Unix(p.ID, 0)
+			m["sysdate"] = t.Format("Jan 2006")
+		}
 		photoMap[p.ID] = m
 	}
+	return photoMap
+}
+
+func GetUserAlbum(albumIDStr string, conn *sql.DB, server *LycheeServer, c *gin.Context) {
+	albumID, err := strconv.Atoi(albumIDStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, fmt.Sprintf("%v", err))
+		return
+	}
+	album, err := GetAlbum(albumID, conn)
+	if err != nil {
+		log.Error("%v", err)
+		c.String(http.StatusBadRequest, fmt.Sprintf("%v", err))
+		return
+	}
+	album.PrepareData(server, conn)
+	photos, err := LoadPhotosOfAlbum(albumID, conn)
+	if err != nil {
+		log.Error("%v", err)
+		c.String(http.StatusInternalServerError, fmt.Sprintf("%v", err))
+		return
+	}
+	photoMap := genPhotoMap(photos)
 	c.JSON(200, gin.H{
 		"id":           album.Id,
 		"title":        album.Title,
@@ -216,4 +271,72 @@ func GetAlbumAction(server *LycheeServer, c *gin.Context) {
 		"thumbs":       album.ThumbUrls,
 		"content":      photoMap,
 	})
+}
+
+func GetSmartAlbum(albumID string, conn *sql.DB, server *LycheeServer, c *gin.Context) {
+	var query string
+	r := map[string]interface{}{}
+	switch id := albumID; id {
+	case "f":
+		r["public"] = "0"
+		query = PhotoSelectStmt + " WHERE start = 1"
+	case "s":
+		r["public"] = "0"
+		query = PhotoSelectStmt + " WHERE public = 1"
+	case "r":
+		r["public"] = "0"
+		ts := time.Now().Unix() - 24*60*60
+		query = PhotoSelectStmt + " WHERE id > " + fmt.Sprintf("%d", ts)
+	case "0":
+		r["public"] = "0"
+		query = PhotoSelectStmt + " WHERE album = 0"
+	}
+	query = query + " ORDER BY " + server.Settings.SortingPhotos
+	rows, err := conn.Query(query)
+	if err != nil {
+		log.Error("%v", err)
+		c.JSON(http.StatusInternalServerError, fmt.Sprintf("%v", err))
+		return
+	}
+	defer rows.Close()
+	var photos []*Photo = make([]*Photo, 0, 0)
+	for rows.Next() {
+		p, err := loadPhotoFromRow(rows)
+		if err != nil {
+			log.Error("%v", err)
+			c.JSON(http.StatusInternalServerError, fmt.Sprintf("%v", err))
+			return
+		}
+		photos = append(photos, p)
+	}
+	if len(photos) == 0 {
+		c.JSON(200, gin.H{
+			"content": false,
+		})
+		return
+	}
+	photoMap := genPhotoMap(photos)
+	c.JSON(200, gin.H{
+		"content": photoMap,
+		"id":      albumID,
+		"num":     len(photos),
+	})
+
+}
+
+func GetAlbumAction(server *LycheeServer, c *gin.Context) {
+	albumID := c.PostForm("albumID")
+	conn, err := server.db.GetConnection()
+	if err != nil {
+		log.Error("%v", err)
+		c.String(http.StatusInternalServerError, "Can't connect to DB")
+		return
+	}
+
+	defer conn.Close()
+	if len(albumID) > 2 {
+		GetUserAlbum(albumID, conn, server, c)
+	} else {
+		GetSmartAlbum(albumID, conn, server, c)
+	}
 }
